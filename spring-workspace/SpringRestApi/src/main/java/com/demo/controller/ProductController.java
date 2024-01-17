64e8c56a29464ca6d6a4ecc2239f4b5aabbacf06
Omkar Ware <123940891+ifeelomii@users.noreply.github.com>
2024-01-17 12:56:41 +0530

    Update ProductController.java

--- spring-workspace/SpringRestApi/src/main/java/com/demo/controller/ProductController.java
+++ spring-workspace/SpringRestApi/src/main/java/com/demo/controller/ProductController.java
@@ -12,11 +12,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.model.Product;
 import com.demo.service.ProductService;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class ProductController {
 
